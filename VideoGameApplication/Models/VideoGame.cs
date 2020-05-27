a020e831837d6b6868044938741ff51141c7dea8
oldGunCoder <57119330+oldGunCoder@users.noreply.github.com>
2020-05-27 16:15:42 -0400

    scanfolding the code and connected to DB

---
+++ VideoGameApplication/Models/VideoGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoGameApplication.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public int MinimumPlayer { get; set; }
+        public int MaximumPlayer { get; set; }
+
+    }
+}
